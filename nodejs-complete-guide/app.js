f6178db9d6e372b9cdbaf723ee2222d09a82845b
zakzakzak <zakiyff@gmail.com>
2026-02-08 14:15:02 +0700

    blocking & non blocking code

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -21,16 +21,17 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () =>{
+        return req.on('end', () =>{
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, (err)=>{
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
-
+    console.log("OKKEE")
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Hello</title><head>');
